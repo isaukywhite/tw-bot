c033f82107d94404e6837713a43d6c56049dcb47
Isac Dev <50426537+isaukywhite@users.noreply.github.com>
2025-01-23 20:42:31 -0300

    adiciona nova fonte de conselhos e expande a lógica de mensagens diárias

--- src/daily_message.py
+++ src/daily_message.py
@@ -2,13 +2,15 @@ import requests
 import random
 
 def daily_message() -> str | None:
-    random_number = random.randint(1, 3)
+    random_number = random.randint(1, 4)
     if random_number == 1:
         return None
     elif random_number == 2:
         return kenye_quote()
-    else:
+    elif random_number == 3:
         return chuck_norris()
+    elif random_number == 4:
+        return advices_sleep()
 
 def kenye_quote():
     url = "https://api.kanye.rest"
@@ -20,4 +22,10 @@ def chuck_norris():
     url = "https://api.chucknorris.io/jokes/random"
     response = requests.get(url)
     advice = response.json()['value']
+    return advice
+
+def advices_sleep():
+    url = "https://api.adviceslip.com/advice"
+    response = requests.get(url)
+    advice = response.json()['slip']['advice']
     return advice
