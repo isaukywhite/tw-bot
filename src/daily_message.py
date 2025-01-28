6a2d570182565d08edf2954952f2088c59bba99e
Isac Dev <50426537+isaukywhite@users.noreply.github.com>
2025-01-28 15:46:12 -0300

    adiciona suporte para carregar mensagens diárias a partir de um arquivo JSON

--- src/daily_message.py
+++ src/daily_message.py
@@ -1,6 +1,7 @@
 from googletrans import Translator
 import requests
 import random
+import json
 
 def translate_text(text: str, target_language: str = "pt") -> str:
     translator = Translator()
@@ -8,15 +9,19 @@ def translate_text(text: str, target_language: str = "pt") -> str:
     return translated.text
 
 def daily_message() -> str | None:
-    random_number = random.randint(1, 4)
+    random_number = random.randint(1, 2)
     if random_number == 1:
         return None
     elif random_number == 2:
-        return kenye_quote()
-    elif random_number == 3:
-        return chuck_norris()
-    elif random_number == 4:
-        return advices_sleep()
+        return from_json()
+    
+def from_json():
+    with open("src/daily_message.json") as file:
+        data = json.load(file)
+        list_messages = data["conselhos"]
+        random_index = random.randint(0, len(list_messages) - 1)
+        return list_messages[random_index]
+        
 
 def kenye_quote():
     url = "https://api.kanye.rest"
@@ -37,8 +42,5 @@ def advices_sleep():
     return translate_text(advice)
 
 if __name__ == "__main__":
-    message = daily_message()
-    if message:
-        print(f"Daily Message: {message}")
-    else:
-        print("No message today.")
+    message = from_json()
+    print(message)
